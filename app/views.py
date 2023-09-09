d53320e20d2375cacbe97588873520ceb9bba545
Oluwatobi Bamidele <tostirma@gmail.com>
2023-09-09 17:25:24 +0100

    initial commit

---
+++ app/views.py
@@ -0,0 +1,27 @@
+# myapp/views.py
+from django.http import JsonResponse
+import datetime
+import pytz
+
+def get_info(request):
+    slack_name = request.GET.get('slack_name')
+    track = request.GET.get('track')
+
+    current_day = datetime.datetime.now(pytz.utc).strftime('%A')
+
+    utc_time = datetime.datetime.now(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": '',
+        "github_repo_url": '',
+        "status_code": 200
+    }
+
+    return JsonResponse(response_data)
